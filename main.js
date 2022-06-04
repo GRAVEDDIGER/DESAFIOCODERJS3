3aad43c67dc367242d083f83f785aa9284b64f73
Adrian Daniel Abadin <aabadin@gmail.com>
2022-06-04 19:46:50 -0300

    mejora en los objetos

--- main.js
+++ main.js
@@ -1,41 +1,44 @@
 class Paciente {
-    constructor(apellido,nombre,calle,numero,cpa,telefono) {
-        this.apellido=apellido
-        this.nombre=nombre
-        this.telefono=telefono
-        this.direccion = { 
+    constructor(apellido, nombre, calle, numero, cpa, telefono, dni) {
+        this.apellido = apellido
+        this.nombre = nombre
+        this.telefono = telefono
+        this.direccion = {
             calle: calle,
             numero: numero,
             cPostal: cpa
         }
-        this.domicilio= `${calle} ${numero} CPA ${cpa}`
+        this.dni = dni
+        this.domicilio = `${calle} ${numero} CPA ${cpa}`
     }
 }
-class Turnos{
-    constructor(ivTurnos,horas,nmProfesional,pac)
-    {
-        this.ivTurnos=ivTurnos
-        this.horas=horas
-        this.nmProfesional=nmProfesional
-        this.nTurnos=parseInt(ivTurnos)*parseInt(horas)
+class Turnos {
+    constructor(ivTurnos, horas, nmProfesional, horaInicio) {
+        this.ivTurnos = ivTurnos
+        this.horas = horas
+        this.nmProfesional = nmProfesional
+        this.turnos = [] //ARRAY DE DNI DE LOS PACIENTES QUE TIENEN TURNOS EL ARRAY NO DEBE SUPERAR IVTURNOS*HORAS
+        this.horaInicio = new Date(horaInicio).getTime() // VER SI ESTO ESTA BIEN NO ESTA PROBADO 
+
     }
+
 }
+
+
 class Historia {
-    constructor (paciente,turnos){
-        this.paciente=paciente
-        this.turnos=turnos
+    constructor(paciente, turnos) {
+        this.paciente = paciente
+        this.turnos = turnos
     }
 }
-let base =[]
-const obj=new Paciente
-for (let i=0;i<2;i++) {
-let ap=prompt("Apellido")
-let nm=prompt("nombre")
-base.push(new Paciente(ap,nm))
-
-
+let base = []
+const obj = new Paciente
+for (let i = 0; i < 2; i++) {
+    let ap = prompt("Apellido")
+    let nm = prompt("nombre")
+    base.push(new Paciente(ap, nm))
 }
-const turnera =new Turnos
-const hc =new Historia(base,turnera)
+const turnera = new Turnos
+turnera.turnos.push(28278216)
+const hc = new Historia(base, turnera)
 console.log(hc)
-
