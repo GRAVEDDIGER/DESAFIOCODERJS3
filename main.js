9a95033f275c5753800babc805c827f24e39660c
Adrian Abadin <aabadin@gmail.com>
2022-06-11 06:48:46 -0300

    Turnera Objeto Terminada

--- main.js
+++ main.js
@@ -46,8 +46,7 @@ class Profesional {
     //metodo que configura un array de objeto con los turnos del profesional
     for (let dia in this.configuracionTurnos.dias) {
       //loop que recorre los dias de la semana
-      console.log(dia);
-      console.log(this.configuracionTurnos.dias[dia]["length"]);
+
       if (this.configuracionTurnos.dias[dia].length > 0) {
         //evalua si el dia de la semana recorrido tiene algun horario asignado
         for (let innerDia in this.configuracionTurnos.dias[dia]) {
@@ -63,7 +62,6 @@ class Profesional {
             new Date().setHours(parseInt(hf), parseInt(mf)) -
               new Date().setHours(parseInt(hi), parseInt(mi))
           ); //se parsean a entero los valores destructuradsos se generan dos dates y se restan me devuelve un date con la diferencia en ms
-          console.log(Math.abs(diferenciaHoras.valueOf()));
           let hor = parseInt(hi);
           let min = parseInt(mi);
           let horario = "";
@@ -73,17 +71,16 @@ class Profesional {
 
             horario +=
               '"' +
-              (hor < 10 ? "0" + hor.toString() : hor.toString()) +
+              (hor < 10 ? "0" + hor.toString().trim() : hor.toString().trim()) +
               ":" +
-              (min < 10 ? "0" + min.toString() : min.toString()) +
+              (min < 10 ? "0" + min.toString().trim() : min.toString().trim()) +
               '"' +
               ":" +
               '"LIBRE",';
             min += intervalo;
           }
           horario =
-            "{" + horario.substring(0, horario.length - 1) + "}";
-          console.log(horario);
+            "{ " + horario.substring(0, horario.length - 1) + "}";
           this.configuracionTurnos.dias[dia].push(JSON.parse(horario)); 
         }
         //aca hay que generar el objeto con los turnos existentes
@@ -125,6 +122,8 @@ const configurarProfesional = () => {
       ? alert("La respuesta debe ser un numero del 0 al 7")
       : definirHoras(opcionSemana);
   }
+
+profesionalObj.generarTurnos()
 };
 const validarHora = () => {
   let regExHora = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
@@ -207,19 +206,73 @@ numero++
 return res;
 }
 const turnosLibres = opcion=>{
-opcion=opcion.substring(4,opcion.length)
-console.log(opcion)
+console.log(profesionalObj.configuracionTurnos.dias[opcion].length)
+let horarios = profesionalObj.configuracionTurnos.dias[opcion][profesionalObj.configuracionTurnos.dias[opcion].length-1] //esto esta mal hay que ver comohacer paraque agarre todoslos array excepto el indice 0
+let libres =[]
+let numero=1
+libres[0]= "0 - Salir"
+for (turno in horarios)
+{
+  if (horarios[turno]=="LIBRE") 
+  {
+    libres.push(numero+" - "+turno+"\n");
+    numero++
+}
 
+}
+console.log(libres) //NO ENTIENDO PORQUE ME PONEUN ESPACIO DESPUES DE LA COMA 
+let opcionWhile=1
+while(opcionWhile !=0)
+{
+  
+  opcionWhile=prompt(`Elija el turno que desea:
+  
+  ${libres}`)
+
+  if (opcionWhile !=0) {    
+    let rta=prompt("DNI del paciente")
+    const turnoSel=libres[opcionWhile].substring(4,libres[opcionWhile].length-1).toString().trim()
+    profesionalObj.configuracionTurnos.dias[opcion][profesionalObj.configuracionTurnos.dias[opcion].length - 1]['"'+turnoSel+'"']=rta;
+    console.log("Turno Asignado")
+    opcionWhile=0;
+      }
+}
+menuPrincipal()
 }
 const asignarTurno = ()=>{
 let atencion= diasAtencion()//opciones en texto para el menu habria que generar tambien un array con las opciones indexadas para que pueda identificar el dia
-let opcion="1"
-while (opcion!=0)
+let opcion=1
+
+while (opcion !=0)
 {
-    opcion=prompt("Elija el dia de atencion:\n0 - Salir\n"+atencion);
-    
-    (opcion !==0) ? turnosLibres(opcion)  :opcion=0
+    opcion=prompt(`Elija el dia de atencion:
+0 - Salir
+${atencion}`); //NO SE PORQUE ME DEJA DESALINEADAS LAS OPCIONES 
+    console.log(atencion)
+console.log("opcion:",atencion[opcion-1].substring(4,atencion[opcion-1].length-1));
+(opcion!=0) ?(turnosLibres(atencion[opcion-1].substring(4,atencion[opcion-1].length-1))) :opcion=0
 }
+
+
+}
+const menuPrincipal = ()=>{
+
+  while (opcion !== 0) {
+    opcion = prompt(`Elija la opcion deseada
+              0 - Salir
+              1 - Configurar un profesional
+              2 - Configurar un paciente
+              3 - Asignar turnos`);
+    opcion = parseInt(opcion);
+    if (opcion == 0) break;
+    isNaN(opcion) || opcion > 3 || opcion < 0
+      ? alert("Opcion incorrecta ingrese un numero del 0 al 3")
+      : opcion == 1
+      ? configurarProfesional()
+      : opcion == 2
+      ? configurarPaciente()
+      : asignarTurno(); //valida que el dato obtenido sea una opcion
+  }
 }
 //////////////////////////////////////////
 // LOGICA PRINCIPAL                     //
@@ -228,29 +281,5 @@ while (opcion!=0)
 const profesionalObj = new Profesional();
 const PacienteObj = new Paciente();
 let opcion = 1;
-profesionalObj.configuracionTurnos.dias.lunes.push(
-  new Horario("15:00", "22:15")
-);
-profesionalObj.configuracionTurnos.dias.miercoles.push(
-  new Horario("15:00", "22:15")
-);
-profesionalObj.configuracionTurnos.ivTurnos = "15";
-profesionalObj.generarTurnos();
-asignarTurno()
 
-// while (opcion !== 0) {
-//   opcion = prompt(`Elija la opcion deseada
-//             0 - Salir
-//             1 - Configurar un profesional
-//             2 - Configurar un paciente
-//             3 - Asignar turnos`);
-//   opcion = parseInt(opcion);
-//   if (opcion == 0) break;
-//   isNaN(opcion) || opcion > 3 || opcion < 0
-//     ? alert("Opcion incorrecta ingrese un numero del 0 al 3")
-//     : opcion == 1
-//     ? configurarProfesional()
-//     : opcion == 2
-//     ? configurarPaciente()
-//     : asignarTurno(); //valida que el dato obtenido sea una opcion
-// }
+menuPrincipal()
